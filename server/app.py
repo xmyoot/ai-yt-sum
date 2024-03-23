da52539f63e399b76a20c30bba85f18eb338bddb
Hector Jimenez <xmyoot@yahoo.com>
2024-03-23 11:33:21 -0700

    separated spaCY as function

--- server/app.py
+++ server/app.py
@@ -1,7 +1,21 @@
 from flask import Flask, jsonify, request
 from youtube_transcript_api import YouTubeTranscriptApi
+# from transformers import T5Tokenizer, T5ForConditionalGeneration
+import spacy
+from spacy_text_summarizer import SpacyTextSummarizer
+from string import punctuation
+from collections import Counter
+from heapq import nlargest
 
+# Hugging Face() T5, BART, etc.) Gensim, spaCy, OpenAI, BERT
 app = Flask(__name__)
+nlp = spacy.load("en_core_web_sm")
+summarizer = SpacyTextSummarizer(nlp)
+
+def process_transcript(transcripts):
+    transcript = ' '.join([item['text'] for item in transcripts])
+    summary = summarizer.summarize_text(transcript)
+    return { "Summarized Video:": summary }
 
 @app.route('/api/serverCheck', methods=['GET'])
 def get_data():
@@ -13,7 +27,8 @@ def get_transcript():
     url = request.json.get('url')
     video_id = url.split('watch?v=')[1]
     transcript = YouTubeTranscriptApi.get_transcript(video_id)
-    return jsonify(transcript)
+    processed_transcript = process_transcript(transcript)
+    return jsonify(processed_transcript)
 
 if __name__ == '__main__':
     app.run(port=5000)
